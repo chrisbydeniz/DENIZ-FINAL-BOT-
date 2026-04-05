871452dccc3fcdc111f2ba4c38ac36ffb493e418
chrisbydeniz <nourelyakineazoul1@gmail.com>
2026-04-05 17:10:13 +0200

    DigixV.js

---
+++ utils/DigixV.js
@@ -0,0 +1,34 @@
+import readline from 'readline'
+
+export default async function deployAsPremium(){
+    const key = "Deniz2025"
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout,
+    })
+    return new Promise((resolve) => {
+        rl.question(' Do you have password for an admin Purchase? y/n ?', (response) => {
+            response = response.toLowerCase()
+            rl.close()
+            if (response == 'y'){
+                rl.question('please type the password here ', (password) => {
+                    rl.close()
+                    if (password === key){
+                        console.log('success')
+                        resolve(true)
+                    } else {
+                        rl.close()
+                        resolve(false)
+                    }
+                })
+            } else if (response === 'n' ){ 
+                rl.close()
+                resolve(false)
+            } else {
+                rl.close()
+                console.log(' you will log without any privilegies.')
+                resolve(false)
+            }
+        })
+    })
+}
