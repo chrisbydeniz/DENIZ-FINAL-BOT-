3db2ab4d4a01096894b8792adba89c5cd9ae22f4
chrisbydeniz <nourelyakineazoul1@gmail.com>
2026-04-05 17:04:32 +0200

    config.js

---
+++ config.js
@@ -0,0 +1,17 @@
+{
+  "users": {
+    "33753191305": {
+      "sudoList": ["33753191305@s.whatsapp.net"],
+      "tagAudioPath": "database/DigiX.mp3",
+      "antilink": true,
+      "response": true,
+      "autoreact": false,
+      "prefix": ".v",
+      "reaction": "👑",
+      "welcome": true,
+      "record": true,
+      "type": true,
+      "publicMode": false
+    }
+  }
+}
